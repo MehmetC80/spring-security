09650b3ff1e8563951c5788d69ee25ef26fbc5fa
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-02-26 12:10:11 +0100

    update JwtService

--- src/main/java/de/memozone/springsecurity/config/JwtService.java
+++ src/main/java/de/memozone/springsecurity/config/JwtService.java
@@ -1,13 +1,82 @@
 package de.memozone.springsecurity.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
 public class JwtService {
-    public String extractUniqueAuthenticationIdentifier(String jwt) {
 
-        return null;
+    private static final String SECRET_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY3NzM5OTA1NiwiaWF0IjoxNjc3Mzk5MDU2fQ.gfJwBHi_ZDa3pJEoYDJLE-eBpYFnAY8NqaRvxnbawGQ";
+
+
+    private Claims extractAllClaims(String jwt){
+
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
     }
+
+    public <T> T extractClaim(String jwt, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(jwt);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+
+
+
+    public String extractUniqueAuthenticationIdentifier(String jwt) {
+        return extractClaim(jwt,Claims::getSubject);
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    public String generateToken(Map<String,Object> extraClaims, UserDetails userDetails){
+
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000*60*24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+ public boolean isTokenValid(String jwt,UserDetails userDetails){
+
+        final String username = extractUniqueAuthenticationIdentifier(jwt);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(jwt);
+ }
+
+    private boolean isTokenExpired(String jwt) {
+        return extractExpiration(jwt).before(new Date());
+    }
+
+    private Date extractExpiration(String jwt) {
+        return extractClaim(jwt,Claims::getExpiration);
+    }
+
+
 }
