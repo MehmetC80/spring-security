0082a21679836bc09e4ad84af7856b9043988ba1
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-02-26 09:01:58 +0100

    update pom.xml: jjwt, update JwtAuthenticationFilter and add JwtService

---
+++ src/main/java/de/memozone/springsecurity/config/JwtService.java
@@ -0,0 +1,13 @@
+package de.memozone.springsecurity.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+    public String extractUniqueAuthenticationIdentifier(String jwt) {
+
+        return null;
+    }
+}
